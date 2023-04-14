40f55cc4d755acd5d248c0bc687425e95b787a98
Martin Nahuel Garcia <nitramleuhan@gmail.com>
2023-04-14 13:59:59 -0300

    Update CLASE_MGyGV_GWAS.R

--- CLASE_MGyGV_GWAS.R
+++ CLASE_MGyGV_GWAS.R
@@ -57,14 +57,6 @@ simulN(Nind, Nmarkers, Nqtl, Esigma, Pmean, Perror)
 population <- nsimout$geno
 colnames(population) <- c(paste("M", 1:Nmarkers,sep = ""))
 rownames(population) <- c(paste("IND", 1:Nind,sep = ""))
-#armamos la matriz de datos genomicos como la requiere GAPIT3
-myGD <- as.data.frame(population)
-myGD <- cbind(feno$IND,myGD)
-colnames(myGD)[1] <- "IND"
-#write.csv(myGD, "myGD.csv")
-#myGD <- as.data.frame(read.csv("myGD.csv", header = T))
-#myGD <- read.csv("myGD.csv", header = T)
-#myGD <- myGD[,-1]
 
 
 #matriz de datos fenotipicos
@@ -73,6 +65,14 @@ colnames(popfeno) <- "PHENO"
 rownames(popfeno) <- c(paste("IND", 1:Nind,sep = ""))
 feno <- data.frame ( IND = rownames(popfeno),
                      Pheno = popfeno)
+#armamos la matriz de datos genomicos como la requiere GAPIT3
+myGD <- as.data.frame(population)
+myGD <- cbind(feno$IND,myGD)
+colnames(myGD)[1] <- "IND"
+#write.csv(myGD, "myGD.csv")
+#myGD <- as.data.frame(read.csv("myGD.csv", header = T))
+#myGD <- read.csv("myGD.csv", header = T)
+#myGD <- myGD[,-1]
 
 
 #mapa
@@ -145,6 +145,85 @@ myGAPIT <- GAPIT(
 
 #Comparar marcadores asociados con los QTLs "reales"
 
+QTL$SNP <- paste0("M",QTL$marker)
+
+#blink
+blink <- read.csv("GAPIT.Blink.PHENO.GWAS.Results.csv",header = T)
+head(blink)
+ablink <- blink[,c(1,9)]
+head(ablink)
+ablink <- ablink[ablink$FDR_Adjusted_P.values <0.05,]
+asoc_blink <- merge(QTL, ablink, by = "SNP")
+
+#FarmCPU
+FarmCPU <- read.csv("GAPIT.FarmCPU.PHENO.GWAS.Results.csv",header = T)
+head(FarmCPU)
+aFarmCPU <- FarmCPU[,c(1,9)]
+head(aFarmCPU)
+aFarmCPU <- aFarmCPU[aFarmCPU$FDR_Adjusted_P.values <0.05,]
+asoc_FarmCPU <- merge(QTL, aFarmCPU, by = "SNP")
+
+#MLMM
+MLMM <- read.csv("GAPIT.MLMM.PHENO.GWAS.Results.csv",header = T)
+head(MLMM)
+aMLMM <- MLMM[,c(1,9)]
+head(aMLMM)
+aMLMM <- aMLMM[aMLMM$FDR_Adjusted_P.values <0.05,]
+asoc_MLMM <- merge(QTL, aMLMM, by = "SNP")
+
+#SUPER
+SUPER <- read.csv("GAPIT.SUPER.PHENO.GWAS.Results.csv",header = T)
+head(SUPER)
+aSUPER <- SUPER[,c(1,9)]
+head(aSUPER)
+aSUPER <- aSUPER[aSUPER$FDR_Adjusted_P.values <0.05,]
+asoc_SUPER <- merge(QTL, aSUPER, by = "SNP")
+
+#MLM
+MLM <- read.csv("GAPIT.MLM.PHENO.GWAS.Results.csv",header = T)
+head(MLM)
+aMLM <- MLM[,c(1,9)]
+head(aMLM)
+aMLM <- aMLM[aMLM$FDR_Adjusted_P.values <0.05,]
+asoc_MLM <- merge(QTL, aMLM, by = "SNP")
+
+#GLM
+GLM <- read.csv("GAPIT.GLM.PHENO.GWAS.Results.csv",header = T)
+head(GLM)
+aGLM <- GLM[,c(1,9)]
+head(aGLM)
+aGLM <- aGLM[aGLM$FDR_Adjusted_P.values <0.05,]
+asoc_GLM <- merge(QTL, aGLM, by = "SNP")
+
+#The statistical power of a hypothesis test is the probability of detecting an effect,
+#if there is a true effect present to detect.
+
+#Entonces podemos estimar el poder estadístico de cada evaluación en nuestros datos
+#simulados de la siguiente manera (ya que conocemos los marcadores con asociación real
+#al caracter, imposible en datos reales):
+#pow <- Marcadores_Asociados_Reales/Nqtl
+
+pow_blink <- dim(asoc_blink)[1]/Nqtl
+pow_FarmCPU <- dim(asoc_FarmCPU)[1]/Nqtl
+pow_MLMM <- dim(asoc_MLMM)[1]/Nqtl
+pow_SUPER <- dim(asoc_SUPER)[1]/Nqtl
+pow_MLM <- dim(asoc_MLM)[1]/Nqtl
+pow_GLM <- dim(asoc_GLM)[1]/Nqtl
+
+data <- matrix(c(
+dim(ablink)[1]  ,   dim(asoc_blink)[1], dim(ablink)[1]-dim(asoc_blink)[1],     pow_blink,
+dim(aFarmCPU)[1], dim(asoc_FarmCPU)[1], dim(aFarmCPU)[1]-dim(asoc_FarmCPU)[1], pow_FarmCPU,
+dim(aMLMM)[1]   ,    dim(asoc_MLMM)[1], dim(aMLMM)[1]-dim(asoc_MLMM)[1],       pow_MLMM,
+dim(aSUPER)[1]  ,   dim(asoc_SUPER)[1], dim(aSUPER)[1]-dim(asoc_SUPER)[1],     pow_SUPER,
+dim(aMLM)[1]    ,     dim(asoc_MLM)[1], dim(aMLM)[1]-dim(asoc_MLM)[1],         pow_MLM,
+dim(aGLM)[1]    ,     dim(asoc_GLM)[1], dim(aGLM)[1]-dim(asoc_GLM)[1],         pow_GLM), ncol=4, byrow=TRUE)
+
+# specify the column names and row names of matrix
+colnames(data) = c('associatedSNP', 'TRUEassociatedSNP', 'FALSEpositives', 'Power')
+rownames(data) <- c('blink', 'FarmCPU', 'MLMM', 'SUPER', 'MLM', 'GLM')
+
+final=as.table(data)
+final
 
 
 
